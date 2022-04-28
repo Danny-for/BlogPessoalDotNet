242fe3916edcbec92687564a1f7622a0e694b800
danny-for <dannygeovanna2019@gmail.com>
2022-04-28 15:02:34 -0300

    @Danny-for [Task:Criação de Tabela com ORM EF] {28/04/2022 15:03}

--- BlogPessoal/Startup.cs
+++ BlogPessoal/Startup.cs
@@ -1,3 +1,4 @@
+using BlogPessoal.src.data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,20 +26,25 @@ namespace BlogPessoal
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            //Configuração Bancos de Dados
             IConfigurationRoot config = new ConfigurationBuilder()
                 .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
                 .AddJsonFile("appsettings.json")
                 .Build();
-                
+            
+            services.AddDbContext<BlogPessoalContexto>(opt => opt.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+
+            //Configuração controlador
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BlogPessoalContexto contexto)
         {
             if (env.IsDevelopment())
             {
+                contexto.Database.EnsureCreated();
                 app.UseDeveloperExceptionPage();
             }
 
---
+++ BlogPessoal/src/modelos/UsuarioModelo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace BlogPessoal.src.modelos
+{
+    [Table("tb_usuarios")]
+    public class UsuarioModelo
+    {
+        //definindo coluna id
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required] //precisa colocar nome
+        [StringLength(50)] 
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Email { get; set; }
+
+        [Required,StringLength(30)]
+        public string Senha { get; set; }
+
+
+        public string Foto { get; set; } 
+
+        [JsonIgnore]
+        public List<PostagemModelo> MinhasPostagens { get; set; }
+
+
+    }
+}
